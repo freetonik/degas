ee073df78ed50f116b40908fda9f3406b81d4627
Rakhim Davletkaliyev <freetonik@gmail.com>
2019-04-04 20:25:05 +0300

    update license

--- project.clj
+++ project.clj
@@ -1,25 +1,21 @@
-(defproject degas "0.2.3"
-  :description "FIXME: write description"
-  :url         "http://example.com/FIXME"
+(defproject degas "0.2.41"
+  :description "Small framework for evolutionary computation in the browser."
+  :url         "https://github.com/freetonik/degas"
   :license     { :name "Eclipse Public License"
-                 :url "http://www.eclipse.org/legal/epl-v10.html" }
+                :url "http://www.eclipse.org/legal/epl-v10.html" }
 
-  :plugins
-  [[lein-cljsbuild "1.1.7"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
 
-  :dependencies
-  [[org.clojure/clojure "1.10.0"]
-   [org.clojure/clojurescript "1.10.520"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520"]]
 
-  :cljsbuild
-  { :builds
-    [{ :id "advanced"
-       :source-paths ["src" "test"]
-       :compiler
-       { :main           degas.core
-         :output-to      "target/main.js"
-         :optimizations  :advanced
-         :source-map     "target/main.js.map"
-        :pretty-print   false }}]}
-
+  :cljsbuild { :builds
+              [{ :id "advanced"
+                :source-paths ["src"]
+                :compiler
+                {
+                 :output-to      "target/main.js"
+                 :optimizations  :advanced
+                 :source-map     "target/main.js.map"
+                 :pretty-print   false }}]}
   )
